ebd9f9f6ccd6cc1104ed359ec5627b5b2ab31480
acdaigneault <71884806+acdaigneault@users.noreply.github.com>
2021-04-02 18:36:08 -0400

    Dimensionnement ailettes quasi-final

--- dimensionnement_HX03_ailettes.m
+++ dimensionnement_HX03_ailettes.m
@@ -9,22 +9,22 @@ clc
 clear all
 
 % --- Chaleur à transférer --- %
-Q = 3.608429791771280e+04; % W Selon les bilans d'énergie
+Q = 39512; % W Selon les bilans d'énergie
 
 % --- Informations sur les débits --- %
-n15 = 17.644070842975005; % mol/s
+n15 = 19.3203; % mol/s
 y15_CH4 = 0.6243;
 y15_CO2 = 1 - y15_CH4;
-n16 = 11.023229704450031; % mol/s
+n16 = 28.9135; % mol/s
 
 % --- Variable posée --- %
-U0 = 150; % W/m2K U approximatif moyen pour transfert gaz-eau (Hall, 2018, p.221)
+U0 = 50; % W/m2K U approximatif pour crossflow eau-air (Bergman, 2018, p. 650)
 
 % --- Températures déterminées et pression --- %
 T15 = 343.15; % K T opération AD-01 (Okoro & Sun, 2019)
 T18 = 288.15; % K T opération AB-01, choix de l'équipe
-T16 = 280.15; % K Moyenne température in de refroidissement (Hall, 2018, p.391)
-T17 = 322.15; % K Température max vers tour de refroissement (Hall, 2018, p.394)
+T16 = 280.15; % K Approximation température in de refroidissement (Hall, 2018, p.391)
+T17 = 298.15; % K Température vers tour de refroissement ou autre unités
 P = 800000; % Pa Pression d'opération de AB-01
 R = 8.314; % m3*Pa/molK
 Tm_gaz = (T15+T18)/2; % Température moyenne du gaz
@@ -40,17 +40,18 @@ Df = (Dto+2*hf); % m Diamètre ailette
 Nf = 315; % Nombre d'ailette par metre de tube (Thermofin, 2020)
 ef = 0.5/1000; % m Epaisseur d'une ailette (Frass, 2015, p.51)
 
-% --- Calcul pertinent des aires --- %
+% --- Calculs pertinents des aires --- %
 At1m = pi*Dto; % m2 Aire d'un tube par 1m;
-Ate1m = At1m - Nf*ef*pi*Dto; % m2 Aire tube sans ailette
-Af1m = Nf*(2*pi*(Df^2 - Dto^2)/4 + ef*pi*Df); % m2 Aire d'une ailette
-Atot1m = Ate1m + Af1m;
+Ate1m = At1m - Nf*ef*pi*Dto; % m2 Aire tube sans ailette pour 1m
+Af1m = Nf*(2*pi*(Df^2 - Dto^2)/4 + ef*pi*Df); % m2 Aire d'une ailette pour 1m
+Atot1m = Ate1m + Af1m; % m2 Aire totale extérieure pour 1m
 Nrangees = 8; % Nombre de rangées (Frass, 2015, p.53)
-espace = 5/1000; % m Espace arbitraire de 5 mm entre les ailettes des tubes
-Ntubes = 60; % 7-8 tubes par rangées
-Ntprmax = 8; % Nombre max de tube pour une rangée
-Hech = Ntprmax*(Df+espace); % Hauteur de l'échangeur avec ailettes collées
+espace = 5/1000; % m Espace horizontal arbitraire de 5 mm entre les ailettes des tubes
+Ntubes = 33; % 7-8 tubes par rangées
+Ntprmax = 5; % Nombre max de tube pour une rangée
+Hech = Ntprmax*(Df+espace); % Hauteur de l'échangeur 
 Lech = Nrangees*sqrt(3)/2*(Df+espace); % Longueur de l'échangeur
+pf = 1/Nf; % m Pitch de l'ailette (distance entre deux ailettes)
 
 % --- Autre infos --- %
 Rfo = 0.0003; % m2C/W Facteur de Fouling max pour de l'eau de refroidissement (Sinnott, 2020, p. 780)
@@ -80,68 +81,84 @@ Q_gaz = n15*M_gaz/rho_gaz; % m3/s Débit volumétrique du biogaz
 
 % --- Propriétés de l'eau de refroidissement et calculs intermédiaires --- %
 M_H2O = 0.018015; % kg/mol Masse molaire
-rho_H2O = 997; % kg/m3 à Tf = 300K Masse volumique (Bergman, 2018, Table A6) 
-Cp_H2O = 4179; % J/KgK à Tf = 300K Cp (Bergman, 2018, Table A6) 
-k_H2O = 613e-3; % W/mK à Tf = 300K Conductivité thermique (Bergman, 2018, Table A6) 
-mu_H2O = 855e-6; % Pa*s à Tf = 300K Viscosité dynamique (Bergman, 2018, Table A6) 
-Pr_H2O = 5.83; % à Tf = 300K  Nombre de Prandtl (Bergman, 2018, Table A6) 
+rho_H2O = 999; % kg/m3 à Tf = 300K Masse volumique (Bergman, 2018, Table A6) 
+Cp_H2O = 4184; % J/KgK à Tf = 300K Cp (Bergman, 2018, Table A6) 
+k_H2O = 598e-3; % W/mK à Tf = 300K Conductivité thermique (Bergman, 2018, Table A6) 
+mu_H2O = 1080e-6; % Pa*s à Tf = 300K Viscosité dynamique (Bergman, 2018, Table A6) 
+Pr_H2O = 7.56; % à Tf = 300K  Nombre de Prandtl (Bergman, 2018, Table A6) 
 Q_H2O = n16*M_H2O/rho_H2O; % m3/s Débit volumétrique 
 
-% --- Calcul du facteur de correction courant croisé --- %
+% --- Méthode eNTU --- %
 Tci = T16;
 Tco = T17;
 Thi = T15;
 Tho = T18;
-R_ = (Thi - Tho)/(Tco - Tci);
-P_ = (Tco - Tci)/(Thi - Tci);
-F = 0.595; % Facteur de correction (Bergman, 2018, fig 11S.3)
 
-% --- Calcul de la différence moyenne de température logarithmique --- %
-dT1 = Thi-Tco; 
-dT2 = Tho-Tci;
-dTlm = (dT2-dT1)/log(dT2/dT1);
+Cc = Q/(Tco-Tci); 
+Ch = Q/(Thi-Tho);
+
+if Cc < Ch
+    Cmin = Cc;
+    Cmax = Ch;
+else 
+    Cmin = Ch;
+    Cmax = Cc;
+end
+
+Cr = Cmin/Cmax;
+e = Q/(Cmin*(Thi-Tci));
+
+% Calcul NTU selon C mixé
+if abs(Cmin - Ch) < 1e-6
+    NTU = -log(Cr*log(1-e)+1)/Cr; % Cmin mixé
+else 
+    NTU = -log(1+log(1-e*Cr)/Cr); % Cmax mixé
+end
 
 % --- Début des intérations avec le U --- %
 U = 0;
 Ucal = U0;
-tol = 5;
+tol = 0.01;
 
 while abs(Ucal - U) > tol
     U = Ucal;
     % --- Calcul de l'aire et de rarrangement des tubes --- %
-    A = Q/(F*Ucal*dTlm) % m2 Surface d'échange entre les fluides
-    %Ntubes = floor(A/(Atot1m*Wech)); % Nombre de tube
-    %Ntpr = floor(Ntubes/Nrangees); % Nombre de tube par rangée
-    Wech = A/(Ntubes*Atot1m)
+    Ao = Cmin*NTU/U;
+    Wech = Ao/(Ntubes*Atot1m);
+    Lt = Wech;
     
     % --- Calcul du coefficient de transfert thermique côté gaz --- %
     Alibre = Wech*(Hech - Ntprmax*(Dto+Nf*Df*ef)); % Aire de passage du gaz
     v_gaz = Q_gaz/Alibre; % m/s 
-    L_gaz = Dto*Atot1m/At1m; % Longueur caractéristique 
+    L_gaz = Dto; % Longueur caractéristique 
     Re_gaz = rho_gaz*v_gaz*L_gaz/mu_gaz; % Nombre de Reynolds
-    Nu_gaz = 0.45*Re_gaz^0.625*Pr_gaz^(1/3)*(Atot1m/At1m)^-0.375; % Nombre de Nusselt (Frauss, 2015, p.12)
+    Nu_gaz = 0.134*Re_gaz^0.681*Pr_gaz^0.33*((pf-ef)/hf)^0.2*(pf/ef)^0.1134; % (Sinnott, 2020, p.891)
     ho = k_gaz*Nu_gaz/L_gaz; % W/m2K h côté gaz
     
-    % Nombre de Reynolds
+    % --- Calcul du coefficient de transfert thermique côté eau --- %
     Q_H2O = n16*M_H2O/rho_H2O; % m3/s 
     v_H2O = Q_H2O/(Ntubes*pi*Dti^2/4); % m/s Vitesse de l'eau dans les tubes
     Re_H2O = rho_H2O*v_H2O*Dti/mu_H2O; 
     Nu_H2O = 0.023*Re_H2O^(4/5)*Pr_H2O^0.3; % (Bergman, 2018, p.519)
     hi = k_H2O*Nu_H2O/Dti;
     
-    Ai = pi*Dti*Wech*Ntubes;
-    Ao = Atot1m*Wech*Ntubes;
-    nf = 0.9;
+    % --- Calcul de l'aire interne --- %
+    Ai = pi*Dti*Lt*Ntubes; % m2 Aire totale dans les tubes
     
-    Ucal = 1/(A*((1/(hi*Ai) + Rfi/Ai + Rfo/Ao + 1/(ho*Ao))/nf + log(Dto/Dti)/(2*pi*k_al*Wech*Ntubes)))
-
+    % --- Calcul de l'éefficacité des ailettes --- %
+    m = sqrt(2*ho/(k_al*ef));
+    nf = tanh(m*hf)/(m*hf); % Efficacité d'une ailette
+    no = 1 - (Af1m/Atot1m)*(1-nf); % Efficacité d'une surface
     
-%     U0 = 1/(1/0.95*(1/ho+1/Rfo)*Atot1m*Wech/Af1m + Dto*log(Dto/Dti)/(2*k_al) + Dto/(Dti*Rfi) + Dto/(Dti*hi));
-%     Ucal = Atot1m*Wech*U0/A
-    
-    
-
-    
-    
-  
+    % --- Recalcul du coefficient de transfert global côté gaz --- %
+    Ucal = 1/((1/ho+Rfo)/no*(At1m/Af1m) + (Dto*log(Dto/Dti))/(2*k_al) + Dto/Dti*(Rfi + 1/hi)); % W/m2K (Sinnott, 2020, p.775 et p.891)
 end
+
+
+fprintf('Hauteur : %.2f m\n', Hech)
+fprintf('Largueur : %.2f m\n', Wech)
+fprintf('Longueur : %.2f m\n', Lech)
+fprintf('U : %.2f W/m2K \n', Ucal)
+fprintf('Surface extérieur : %.2f m2\n', Ao)
+fprintf('Coefficient de transfert : %.2f W/m2K\n', ho)
+
